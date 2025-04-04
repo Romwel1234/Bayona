f57532647195c5f2e1173d821be5d3c0e8a11470
Romwel <romwelbayona3@gmail.com>
2025-04-04 12:53:09 +0800

    first commit

--- database/migrations/2025_04_04_041509_create_roles_table.php
+++ database/migrations/2025_04_04_041509_create_roles_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->UnsignedBigInteger ('role_id');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('role_name');
+            $table->string('url');
         });
     }
 
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
